bcb4cdec7bde4a810e9e7f767129c8888bdeaf85
Yansell Rivas Diaz <yrivas@syneteksolutions.com>
2020-08-10 15:22:42 +0000

    fix(AuthorizeNetBundle) Symfony 4 support
     * Fix dependency injection Root Node compatibility with symfony 4

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -17,8 +17,12 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('authorizenet');
+        $treeBuilder = new TreeBuilder('authorizenet');
+        if (method_exists($treeBuilder, 'getRootNode')) {
+            $rootNode = $treeBuilder->getRootNode();
+        } else {
+            $rootNode = $treeBuilder->root('authorizenet');
+        }
 
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
