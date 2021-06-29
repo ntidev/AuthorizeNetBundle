d403bd4ba2abe4b42f122fad87faf617f8be7bd1
Benjamín Visón <bvisonl@gmail.com>
2021-06-29 17:53:24 -0400

    Delete authorizedotnet_craue_config_setting.sql

--- SQL/authorizedotnet_craue_config_setting.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO `craue_config_setting` (`name`, `section`, `value`) VALUES ('authorizedotnet.login_id', 'Authorize.NET', '548xu8pp2RKZ');
-INSERT INTO `craue_config_setting` (`name`, `section`, `value`) VALUES ('authorizedotnet.transaction_key', 'Authorize.NET', '8Agx83gDB4Az7227');
-INSERT INTO `craue_config_setting` (`name`, `section`, `value`) VALUES ('authorizedotnet.environment', 'Authorize.NET', 'Sandbox');
